3e57d0e30abdeefd4573ea0f9e4952b9801021d4
dealerup <alexoliveira7x@gmail.com>
2020-04-10 20:20:07 -0300

    propriedades

---
+++ properties.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Pegar primeiro elemento de um Array
+  List array = ['Arroz', 'Feijão', 'Batata', 'O que falta?'];
+  List array2 = array;
+  List array3 = List.from(array);
+  List array4 = [];
+
+  //array.first vai retornar arroz
+  //array.last vai retornar arroz
+  //array.length vai retornar arroz
+  //array.reversed vai trazer os elementos de trás pra frente
+  //array3.isEmpty
+  //array3.isNotEmpty ou !array3.isEmpty
+  //runtimeType é igual o typeof do Javascript, mostra o tipo da variavel
+  //array2.hashCode == other => Verifica se esta apontando pro mesmo objeto
+  //array.single verifica se tem um elemento e retorna o valor deste um.
+  // try {
+  //   print(array.single);
+  // } catch (e) {
+  //   print('Array tem muitos elementos');
+  // }
+
+
+}
